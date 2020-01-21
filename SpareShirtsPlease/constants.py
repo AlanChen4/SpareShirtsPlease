5e7adb2b8c77ce98c6dd13b82aa2669eea15236f
Alan Chen <avchen4@gmail.com>
2020-01-21 16:45:46 -0500

    Added feature so that all rows (up to one million) are returned as a list. Also began the unittest development

--- SpareShirtsPlease/constants.py
+++ SpareShirtsPlease/constants.py
@@ -1,3 +1,4 @@
+# these are used to access proper ranges in spreadsheet
 SHEET_ID = '1Vj-kCqsmsa8Ssk_ZAXJiFOmlrK_oiWUl9_pPSQKNoc8'
-RANGE_NAME = 'A1:A3'
+RANGE_NAME = 'A1:A1000000'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
---
+++ tests/test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class SpareShirtsTests(unittest.TestCase):
+
+	def test_spreadsheet(self):
+		'''this should transform the spreadsheet values into a .csv file'''
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
